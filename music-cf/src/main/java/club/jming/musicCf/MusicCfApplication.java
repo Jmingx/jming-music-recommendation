bf076f6c5e8bc0e8fae40aa48687899f64d9f4ab
JMing <782893110@qq.com>
2022-04-13 01:34:29 +0800

    添加功能：播放记录得分+搭建music-cf、music-server环境(引入kafka)

---
+++ music-cf/src/main/java/club/jming/musicCf/MusicCfApplication.java
@@ -0,0 +1,13 @@
+package club.jming.musicCf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicCfApplication.class, args);
+    }
+
+}
---
+++ music-cf/src/test/java/club/jming/musicCf/MusicCfApplicationTests.java
@@ -0,0 +1,13 @@
+package club.jming.musicCf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
